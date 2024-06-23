ca94e9fb65e1ef68220f625cbd7f09900f1d2ffe
Richard "O'Neill <richieoneill88@gmail.com>
2024-06-23 22:54:18 +0000

    create content for shirt_list.html

--- shirts/views.py
+++ shirts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Shirt
 
-def shirt_list(request):
+def shirts_list(request):
     shirts = Shirt.objects.all()
     return render(request, 'shirts/shirt_list.html', {'shirts': shirts})
